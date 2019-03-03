5d72ca8ca6d953af28188c3f04fef62548039ca3
Elayamani K <k.elayamani@gmail.com>
2019-03-03 12:43:37 -0800

    Updates

---
+++ precache-manifest.b16a88f9a21edc6ed304235c9fcafe0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wealthica-portfolio-addon/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cd0ef51b137d7dffb29b",
+    "url": "/wealthica-portfolio-addon/static/js/runtime~main.cd0ef51b.js"
+  },
+  {
+    "revision": "72b97d214e7ba4aced33",
+    "url": "/wealthica-portfolio-addon/static/js/main.72b97d21.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/wealthica-portfolio-addon/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "72b97d214e7ba4aced33",
+    "url": "/wealthica-portfolio-addon/static/css/main.0f2c53e1.chunk.css"
+  },
+  {
+    "revision": "63b388a1b52110ecc02e013517adf69d",
+    "url": "/wealthica-portfolio-addon/index.html"
+  }
+];
