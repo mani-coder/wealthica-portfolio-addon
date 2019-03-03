6bf077d456a48755bc2cd3bbdaf3a4a43e69a044
Elayamani K <k.elayamani@gmail.com>
2019-03-03 13:16:08 -0800

    Use type script

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Wealthica React Portfolio add using highcharts.
           </p>
           <a
             className="App-link"
