1b7ceccec7988e32f5d9c9dae64eaf9f58bdf3ea
Elayamani K <k.elayamani@gmail.com>
2019-03-04 01:31:38 -0800

    Fix utilities

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,26 @@
-import { PortfolioData } from './types';
-import moment, { Moment } from 'moment';
-import { DATE_FORMAT } from './constants';
-
+import { PortfolioData } from "./types";
+import moment, { Moment } from "moment";
+import { DATE_FORMAT } from "./constants";
 
 export const isValidPortfolioData = (data: PortfolioData): boolean => {
-    return Boolean(data.deposit || data.income || data.interest || data.value || data.withdrawal);
+  return Boolean(
+    data.deposit ||
+      data.income ||
+      data.interest ||
+      data.value ||
+      data.withdrawal
+  );
 };
-
 
 export const getDate = (date: string): Moment => {
-    return moment(date.slice(0, 10), DATE_FORMAT);
+  return moment(date.slice(0, 10), DATE_FORMAT);
 };
 
-
-export const getCurrencyInCAD = (date: Moment, value: number, currencyCache: any): number => {
-    return value / currencyCache.get(date.format(DATE_FORMAT));
-}
+export const getCurrencyInCAD = (
+  date: Moment,
+  value: number,
+  currencyCache: any
+): number => {
+  const multiplier = currencyCache[date.format(DATE_FORMAT)];
+  return multiplier ? value / multiplier : value;
+};
