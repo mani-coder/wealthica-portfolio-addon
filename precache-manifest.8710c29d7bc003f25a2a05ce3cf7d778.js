414386e3121b8e5fe57952fd951e14bd9912819b
Elayamani K <k.elayamani@gmail.com>
2019-03-10 18:08:48 -0700

    Updates

--- precache-manifest.8710c29d7bc003f25a2a05ce3cf7d778.js
+++ precache-manifest.8710c29d7bc003f25a2a05ce3cf7d778.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/wealthica-portfolio-addon/static/js/runtime~main.cd0ef51b.js"
   },
   {
-    "revision": "d734783e1fee50c4f5c1",
-    "url": "/wealthica-portfolio-addon/static/js/main.d734783e.chunk.js"
+    "revision": "27cc97e4fb8f2fd1ed1f",
+    "url": "/wealthica-portfolio-addon/static/js/main.27cc97e4.chunk.js"
   },
   {
     "revision": "3b180dd8c60cf8200756",
     "url": "/wealthica-portfolio-addon/static/js/2.3b180dd8.chunk.js"
   },
   {
-    "revision": "d734783e1fee50c4f5c1",
+    "revision": "27cc97e4fb8f2fd1ed1f",
     "url": "/wealthica-portfolio-addon/static/css/main.519258aa.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/wealthica-portfolio-addon/static/css/2.2b092928.chunk.css"
   },
   {
-    "revision": "422dac8628013bb5bf2ed55b52ab339d",
+    "revision": "faab3f6404bf3b5780737dcf8740972e",
     "url": "/wealthica-portfolio-addon/index.html"
   }
 ];
