43707d3e27900ac5586a645e3d222177d15802ca
Elayamani K <k.elayamani@gmail.com>
2019-03-12 00:39:07 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wealthica-portfolio-addon/precache-manifest.b8903724f44d10a62d32aaac559adc0b.js"
+  "/wealthica-portfolio-addon/precache-manifest.c7151591b99867c23ea4e313c15feda8.js"
 );
 
 workbox.clientsClaim();
