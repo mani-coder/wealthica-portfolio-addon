5bc39131d9902d6fd411ff7d3ae901aff4f8bd69
Elayamani K <k.elayamani@gmail.com>
2019-05-19 23:13:29 -0700

    Fix all.

--- src/components/StockTimeline.tsx
+++ src/components/StockTimeline.tsx
@@ -1,7 +1,11 @@
+import _ from 'lodash';
 import moment from 'moment';
 import React, { Component } from 'react';
 import Loader from 'react-loader-spinner';
+import { TYPE_TO_COLOR } from '../constants';
 import { Position } from '../types';
+import Charts from './Charts';
+import { formatCurrency } from '../utils';
 
 type Props = {
   symbol: string;
@@ -19,10 +23,12 @@ class StockTimeline extends Component<Props, State> {
     this.state = {
       loading: true,
     };
-    console.log(props);
+    this._mounted = false;
   }
+  _mounted: boolean;
 
   componentDidMount() {
+    this._mounted = true;
     this.fetchData();
   }
 
@@ -31,78 +37,224 @@ class StockTimeline extends Component<Props, State> {
       this.fetchData();
     }
   }
+  componentWillUnmount() {
+    this._mounted = false;
+  }
 
   fetchData() {
+    if (!this._mounted) {
+      return;
+    }
+
     this.setState({ loading: true });
-    const startDate = this.props.position.transactions[0].date;
+    const startDate = this.props.position.transactions[0].date.clone().subtract('months', 1);
+    const endDate = moment()
+      .startOf('day')
+      .unix();
 
     const url = `https://query1.finance.yahoo.com/v7/finance/chart/${
       this.props.symbol
-    }?period1=${startDate.unix()}&period2=${moment().unix()}&interval=1d&events=history`;
-    console.log('URL to fetch --', url);
+    }?period1=${startDate.unix()}&period2=${endDate}&interval=1d&events=history`;
+
+    console.log('Fetching stock data..', url);
 
     fetch(`https://cors-anywhere.herokuapp.com/${url}`, {
-      // method: 'GET',
-      // mode: 'cors',
-      // referrer: 'https://ca.finance.yahoo.com',
-      // cache: 'force-cache',
+      cache: 'force-cache',
       headers: {
         'Content-Type': 'application/json',
       },
     })
       .then(response => response.json())
       .then(response => {
-        this.setState({ loading: false, data: response });
-        console.log(response);
+        if (this._mounted) {
+          this.setState({ loading: false, data: response });
+        }
       })
       .catch(error => console.log(error));
   }
 
   getSeries(): any {
-    // const data = this.state.data.map(portfolio => {
-    //   return {
-    //     // x: ,
-    //     y: ((portfolio.value - portfolio.deposits) / portfolio.deposits) * 100,
-    //     pnlValue: this.props.isPrivateMode
-    //       ? '-'
-    //       : formatCurrency(portfolio.value - portfolio.deposits, 2).toLocaleString(),
-    //   };
-    // });
+    const timestamps = this.state.data.chart.result[0].timestamp;
+    const closePrices = this.state.data.chart.result[0].indicators.quote[0].close;
+
+    const data: { x: number; y: number }[] = [];
+    // let minPrice, maxPrice, minTimestamp, maxTimestamp;
+    timestamps.forEach((timestamp, index) => {
+      data.push({
+        x: timestamp * 1000,
+        y: closePrices[index],
+      });
+      // if (index === 0) {
+      //   maxPrice = minPrice = closePrices[index];
+      //   minTimestamp = maxTimestamp = timestamp;
+      // }
+      // if (closePrices[index] < minPrice) {
+      //   minPrice = closePrices[index];
+      //   minTimestamp = timestamp;
+      // }
+      // if (closePrices[index] > maxPrice) {
+      //   maxPrice = closePrices[index];
+      //   maxTimestamp = timestamp;
+      // }
+    });
+    // console.log(minTimestamp, maxTimestamp);
+
     return [
+      {
+        id: 'dataseries',
+        name: this.props.symbol,
+        data,
+        type: 'spline',
+
+        tooltip: {
+          valueDecimals: 2,
+        },
+      },
       // {
-      //   id: 'dataseries',
-      //   name: 'P/L %',
-      //   data: data,
-      //   tooltip: {
-      //     valueDecimals: 2,
-      //   },
-      //   type: 'line',
-      // },
-      // {
+      //   name: 'High/Low',
+      //   shape: 'squarepin',
       //   type: 'flags',
-      //   name: 'Max Gain/Loss',
+      //   onSeries: 'dataseries',
+      //   width: 25,
+
       //   data: [
       //     {
-      //       ...min(data, 'y'),
+      //       x: minTimestamp * 1000,
       //       title: 'L',
-      //       text: 'Max Loss',
+      //       text: 'Low Price',
       //     },
       //     {
-      //       ...max(data, 'y'),
-      //       title: 'G',
-      //       text: 'Max Loss',
+      //       x: maxTimestamp * 1000,
+      //       title: 'H',
+      //       text: 'High Price',
       //     },
       //   ],
-      //   onSeries: 'dataseries',
-      //   shape: 'squarepin',
-      //   width: 16,
+      // color: 'purple',
+      // fillColor: 'purple',
+      // style: {
+      //   color: 'white',
       // },
+      // },
+      this.getFlags('buy'),
+      this.getFlags('sell'),
+      this.getFlags('income'),
+      this.getFlags('dividend'),
+      this.getFlags('distribution'),
+      this.getFlags('tax'),
+      this.getFlags('fee'),
     ];
   }
 
+  getFlags = (type: string): any => {
+    const isBuySell = ['buy', 'sell'].includes(type);
+
+    return {
+      name: _.startCase(type),
+      shape: 'squarepin',
+      type: 'flags',
+      onSeries: 'dataseries',
+      width: 25,
+
+      tooltip: {
+        pointFormat: '<b>{point.text}</b>',
+        valueDecimals: 2,
+        split: true,
+      },
+
+      data: this.props.position.transactions
+        .filter(t => t.type === type)
+        .map(transaction => {
+          return {
+            x: transaction.date.valueOf(),
+            title: isBuySell ? transaction.shares : type.charAt(0).toUpperCase(),
+            text: isBuySell
+              ? `${transaction.shares}@${transaction.price}`
+              : `${_.startCase(type)}: $${formatCurrency(transaction.amount, 2)}`,
+          };
+        }),
+      color: TYPE_TO_COLOR[type],
+      fillColor: TYPE_TO_COLOR[type],
+      style: {
+        color: 'white', // text style
+      },
+    };
+  };
+
+  getOptions(): Highcharts.Options {
+    return {
+      title: {
+        text: `${this.props.symbol}`,
+      },
+      rangeSelector: {
+        selected: 5,
+      },
+
+      scrollbar: {
+        barBackgroundColor: 'gray',
+        barBorderRadius: 7,
+        barBorderWidth: 0,
+        buttonBackgroundColor: 'gray',
+        buttonBorderWidth: 0,
+        buttonBorderRadius: 7,
+        trackBackgroundColor: 'none',
+        trackBorderWidth: 1,
+        trackBorderRadius: 8,
+        trackBorderColor: '#CCC',
+      },
+
+      yAxis: [
+        {
+          opposite: false,
+          plotLines: [
+            {
+              value: 0,
+              width: 1,
+              color: 'silver',
+            },
+          ],
+        },
+        {
+          linkedTo: 0,
+        },
+      ],
+      tooltip: {
+        pointFormat: '{series.name}: <b>${point.y}',
+        valueDecimals: 2,
+        split: true,
+      },
+      responsive: {
+        rules: [
+          {
+            condition: {
+              maxWidth: 500,
+            },
+            chartOptions: {
+              chart: {
+                height: 300,
+              },
+              subtitle: {
+                text: undefined,
+              },
+              navigator: {
+                enabled: false,
+              },
+            },
+          },
+        ],
+      },
+      series: this.getSeries(),
+    };
+  }
+
   render() {
-    console.log(this.props);
-    return this.state.loading ? <Loader type="Circles" color="#7f3eab" height="75" width="75" /> : <></>;
+    console.log('Props -- ', this.props, this.state);
+    return this.state.loading ? (
+      <div style={{ textAlign: 'center', margin: '12px' }}>
+        <Loader type="Circles" color="#7f3eab" height="75" width="75" />
+      </div>
+    ) : (
+      <Charts constructorType={'stockChart'} options={this.getOptions()} />
+    );
   }
 }
 
--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,12 @@
-export const DATE_FORMAT = "YYYY-MM-DD";
-export const TRANSACTIONS_FROM_DATE = "2010-01-01";
+export const DATE_FORMAT = 'YYYY-MM-DD';
+export const TRANSACTIONS_FROM_DATE = '2010-01-01';
+
+export const TYPE_TO_COLOR = {
+  buy: 'purple',
+  sell: '#FF897C',
+  income: 'green',
+  dividend: 'green',
+  distribution: 'green',
+  tax: 'brown',
+  fee: 'brown',
+};
