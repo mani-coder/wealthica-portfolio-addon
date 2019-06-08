e631a2ab4513611f8e4c26600f36fc05faa65e7b
Elayamani K <mani@zerodown.com>
2019-06-07 23:30:59 -0700

    Updates

--- precache-manifest.7e3183c0bbaf0bc8ee57aefd07589df6.js
+++ precache-manifest.7e3183c0bbaf0bc8ee57aefd07589df6.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/wealthica-portfolio-addon/static/js/runtime~main.cd0ef51b.js"
   },
   {
-    "revision": "2623af22e036b6f2771b",
-    "url": "/wealthica-portfolio-addon/static/js/main.2623af22.chunk.js"
+    "revision": "02e3691d97f5e12b3ea0",
+    "url": "/wealthica-portfolio-addon/static/js/main.02e3691d.chunk.js"
   },
   {
     "revision": "4ba82e20c730827e243c",
     "url": "/wealthica-portfolio-addon/static/js/2.4ba82e20.chunk.js"
   },
   {
-    "revision": "2623af22e036b6f2771b",
+    "revision": "02e3691d97f5e12b3ea0",
     "url": "/wealthica-portfolio-addon/static/css/main.a9666248.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/wealthica-portfolio-addon/static/css/2.a7772683.chunk.css"
   },
   {
-    "revision": "08b147fb072311de8217c9cab4cd689b",
+    "revision": "43fe19f82ae73a2b42386bebfdbb0422",
     "url": "/wealthica-portfolio-addon/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wealthica-portfolio-addon/precache-manifest.72a2e74f65ca7c08f97145e39359462c.js"
+  "/wealthica-portfolio-addon/precache-manifest.7e3183c0bbaf0bc8ee57aefd07589df6.js"
 );
 
 workbox.clientsClaim();
