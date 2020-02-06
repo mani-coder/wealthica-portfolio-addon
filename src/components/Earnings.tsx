979e9d7f2072b1b4e3312085af819ed81ea42173
Elayamani K <mani@zerodown.com>
2020-02-06 00:04:36 -0800

    Earnings table

--- src/components/Earnings.tsx
+++ src/components/Earnings.tsx
@@ -1,5 +1,189 @@
-import React from 'react';
+import { Select, Table, Typography } from 'antd';
+import moment from 'moment';
+import React, { useEffect, useState } from 'react';
+import Collapsible from 'react-collapsible';
+import { Flex, Box } from 'rebass';
+import { Position } from '../types';
+import { getSymbol } from '../utils';
 
-export default () => {
-  return <></>;
+type Props = {
+  positions: Position[];
+};
+
+export default (props: Props) => {
+  const [loading, setLoading] = useState();
+  const [document, setDocument] = useState();
+  const [symbol, setSymbol] = useState();
+
+  useEffect(() => {
+    if (!props.positions) {
+      return;
+    }
+
+    setLoading(true);
+
+    const content = {
+      sortType: 'asc',
+      entityIdType: 'earnings',
+      sortField: 'startdatetime',
+      includeFields: [
+        'ticker',
+        'companyshortname',
+        'startdatetime',
+        'startdatetimetype',
+        'epsestimate',
+        'epsactual',
+        'epssurprisepct',
+      ],
+      query: {
+        operator: 'and',
+        operands: [
+          { operator: 'gte', operands: ['startdatetime', moment().format('YYYY-MM-DD')] },
+          {
+            operator: 'lt',
+            operands: [
+              'startdatetime',
+              moment()
+                .add(6, 'months')
+                .format('YYYY-MM-DD'),
+            ],
+          },
+          {
+            operator: 'or',
+            operands: props.positions.map(position => ({
+              operator: 'eq',
+              operands: ['ticker', getSymbol(position.security)],
+            })),
+          },
+        ],
+      },
+      offset: 0,
+      size: 200,
+    };
+
+    fetch('https://cors-anywhere.herokuapp.com/https://query2.finance.yahoo.com/v1/finance/visualization', {
+      cache: 'force-cache',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(content),
+    })
+      .then(response => response.json())
+      .then(response => {
+        console.log('earnings', response);
+        const document = response.finance.result[0].documents[0];
+        setDocument(document);
+      })
+      .catch(error => console.log(error))
+      .finally(() => setLoading(false));
+  }, [props.positions]);
+
+  function data() {
+    if (!document) {
+      return [];
+    }
+
+    return (document.rows || [])
+      .map((row, idx) =>
+        document.columns.reduce(
+          (hash, col, index) => {
+            let value = row[index];
+            if (col.id === 'startdatetime') {
+              value = moment(value.slice(0, 10), 'YYYY-MM-DD').format('MMM DD, YYYY');
+            } else if (col.id === 'startdatetimetype') {
+              value =
+                value === 'TNS'
+                  ? 'Time Not Supplied'
+                  : value === 'BMO'
+                  ? 'Before Market Open'
+                  : value === 'AMC'
+                  ? 'After Market Close'
+                  : value;
+            }
+            hash[col.id] = value;
+            return hash;
+          },
+          { key: idx },
+        ),
+      )
+      .filter(row => !symbol || row.ticker === symbol);
+  }
+
+  function columns() {
+    if (!document) {
+      return [];
+    }
+
+    return [
+      {
+        title: 'Stock',
+        key: 'ticker',
+        dataIndex: 'ticker',
+        // ...getColumnSearchProps('ticker'),
+      },
+      {
+        title: 'Company',
+        key: 'companyshortname',
+        dataIndex: 'companyshortname',
+        // ...getColumnSearchProps('companyshortname'),
+      },
+      {
+        title: 'Earnings Date',
+        key: 'startdatetime',
+        dataIndex: 'startdatetime',
+      },
+      {
+        title: 'Earnings Time',
+        key: 'startdatetimetype',
+        dataIndex: 'startdatetimetype',
+      },
+      {
+        title: 'EPS Estimate',
+        key: 'epsestimate',
+        dataIndex: 'epsestimate',
+      },
+    ];
+  }
+
+  const symbols = props.positions
+    .map(position => getSymbol(position.security))
+    .sort()
+    .map((symbol, index) => (
+      <Select.Option key={index} value={symbol}>
+        {symbol}
+      </Select.Option>
+    ));
+
+  return (
+    <Collapsible trigger="Upcoming Earnings" open>
+      <Table
+        title={() => (
+          <Flex flexDirection="row" flexWrap="wrap">
+            <Typography.Title style={{ textAlign: 'center' }} level={4}>
+              Search for a stock in your protofolio:
+            </Typography.Title>
+            <Box p={2} />
+            <Select
+              showSearch
+              value={symbol}
+              placeholder="Enter a stock, e.g: FB, SHOP.TO"
+              showArrow
+              style={{ width: 500 }}
+              onChange={setSymbol}
+              filterOption={(inputValue, option) =>
+                (option!.props!.value! as string).toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+              }
+            >
+              {symbols}
+            </Select>
+          </Flex>
+        )}
+        rowKey="key"
+        loading={loading}
+        dataSource={data()}
+        columns={columns()}
+      />
+    </Collapsible>
+  );
 };
