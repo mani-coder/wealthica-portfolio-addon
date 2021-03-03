889cc6af3bdce1aae79ea18d48776d106876ffaf
Elayamani K <mani@zerodown.com>
2021-03-02 23:32:09 -0800

    remove exports

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -1,14 +1,13 @@
-import * as Highcharts from 'highcharts/highstock';
-import HC_exporting from 'highcharts/modules/exporting';
-import HC_ExportData from 'highcharts/modules/export-data';
-import HC_DrillDown from 'highcharts/modules/drilldown';
+// import HC_exporting from 'highcharts/modules/exporting';
+// import HC_ExportData from 'highcharts/modules/export-data';
+// import HC_DrillDown from 'highcharts/modules/drilldown';
 import HighchartsReact from 'highcharts-react-official';
-
+import * as Highcharts from 'highcharts/highstock';
 import React, { Component } from 'react';
 
-HC_exporting(Highcharts);
-HC_ExportData(Highcharts);
-HC_DrillDown(Highcharts);
+// HC_exporting(Highcharts);
+// HC_ExportData(Highcharts);
+// HC_DrillDown(Highcharts);
 
 type Props = {
   options: Highcharts.Options;
