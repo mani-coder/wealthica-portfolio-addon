f1c77266768f3536d6ba2cd17668c94dcbd62f96
Elayamani K <mani@zerodown.com>
2021-04-03 16:13:06 -0700

    clean up analytics

--- src/analytics.tsx
+++ src/analytics.tsx
@@ -1,5 +1,24 @@
+let enabled = false;
+
+export function initTracking(userHash) {
+  if (!window.analytics || process.env.NODE_ENV !== 'production') {
+    return;
+  }
+
+  enabled = true;
+  window.analytics.identify(userHash);
+  trackEvent('init');
+  trackPage();
+}
+
+export function trackPage() {
+  if (enabled) {
+    window.analytics.page();
+  }
+}
+
 export function trackEvent(action: string, data?: any) {
-  if (window.analytics && process.env.NODE_ENV === 'production') {
+  if (enabled) {
     window.analytics.track(action, data || {});
   } else {
     console.debug('[analytics] track event', { action, data });
